4d717b4c332ddbd493e3b9529f848a5d627ad4cd
Sean <seancyrill@gmail.com>
2023-06-16 13:58:19 +0800

    Update Attribution.jsx

--- src/Attribution.jsx
+++ src/Attribution.jsx
@@ -4,7 +4,7 @@ const Attribution = () => {
   return (
     <div className="attribution">
       <img src="./favicon-32x32.png" alt="" />
-      <p>Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. Coded by <a href="#">Sean</a>.</p>
+      <p>Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="https://github.com/seancyrill/interactive-comments-section">Frontend Mentor</a>. Coded by <a href="#">Sean</a>.</p>
     </div>
   )
 }
